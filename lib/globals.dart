8bb586dcf01d56d8961e4cac813ad6ed9bf3a6fa
EricApostal <60072374+EricApostal@users.noreply.github.com>
2023-05-18 11:19:37 -0400

    fixed globals hopefully

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,7 @@
 library globals;
 
 String spreadsheetId = '1JF3wS10ayFZISBne_MuluZb0MkV-fzrJWAcGdgN4_N8';
-bool isSignedIn = false;
+bool isCheckedIn= false;
 int currentSignedInRow = 0;
 
 List globalTools = [];
